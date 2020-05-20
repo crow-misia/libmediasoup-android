21a0c750f4fa884670650ef9a063b5561beadf9c
Zenichi Amano <crow.misia@gmail.com>
2020-05-20 18:21:36 +0900

    bintrayプラグインバージョンアップ

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,12 +1,11 @@
 object Versions {
     const val kotlin = "1.3.72"
     const val gradlePlugin = "3.6.3"
-    const val bintrayPlugin = "1.8.4"
+    const val bintrayPlugin = "1.8.5"
 
     const val ndk = "21.1.6352462"
     const val buildTools = "29.0.3"
     const val compileSdk = 29
-    const val targetSdk = 29
     const val minSdk = 16
 
     const val core = "0.0.2"
