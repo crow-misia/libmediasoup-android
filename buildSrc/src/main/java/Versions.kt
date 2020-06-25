f2007cb260ab3a40bcedb2888e95ca65f6e41656
Zenichi Amano <crow.misia@gmail.com>
2020-06-25 22:15:47 +0900

    libwebrtc 84.4147@{#10}にバージョンアップ AndroidStudio 4.0 / NDK 21.3.6528147 にアップデート

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,20 +1,20 @@
 object Versions {
     const val kotlin = "1.3.72"
-    const val gradlePlugin = "3.6.3"
+    const val gradlePlugin = "4.0.0"
     const val bintrayPlugin = "1.8.5"
 
-    const val ndk = "21.1.6352462"
-    const val buildTools = "29.0.3"
+    const val ndk = "21.3.6528147"
+    const val buildTools = "30.0.0"
     const val compileSdk = 29
     const val minSdk = 16
 
-    const val core = "0.0.2"
+    const val core = "0.0.3"
 
     const val coroutines = "1.3.5"
 
     const val moshi = "1.9.2"
 
-    const val webrtc = "83.12.0"
+    const val webrtc = "84.10.0"
     const val webrtcKtx = "0.0.1"
     const val sdp = "0.6.0"
 
