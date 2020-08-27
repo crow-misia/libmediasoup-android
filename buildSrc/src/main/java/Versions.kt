a24d64eb47647e01d9b96df22829a8533aaea01f
Zenichi Amano <crow.misia@gmail.com>
2020-08-28 00:58:13 +0900

    kotlin / テストライブラリアップデート

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,19 +1,19 @@
 object Versions {
-    const val kotlin = "1.3.72"
+    const val kotlin = "1.4.0"
     const val gradlePlugin = "4.0.1"
     const val bintrayPlugin = "1.8.5"
 
     const val ndk = "21.3.6528147"
     const val buildTools = "30.0.1"
-    const val compileSdk = 29
+    const val compileSdk = 30
     const val minSdk = 16
 
-    const val core = "0.1.1"
+    const val core = "0.1.2"
 
     const val webrtcKtx = "0.0.2"
 
     const val junit = "4.13"
     const val assertk = "0.22"
-    const val extJunit = "1.1.1"
-    const val espresso = "3.2.0"
+    const val extJunit = "1.1.2"
+    const val espresso = "3.3.0"
 }
