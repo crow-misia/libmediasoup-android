9989470d2ec2ace65697fb44c3fd30b363011cf4
ishabodiwala <ishabodiwala@gmail.com>
2022-06-07 18:09:43 +0530

    add RtpParameters.Encoding and degradationPref in appData

--- core/src/main/java/org/webrtc/RTCUtils.kt
+++ core/src/main/java/org/webrtc/RTCUtils.kt
@@ -1,11 +1,41 @@
 package org.webrtc
 
-internal object RTCUtils {
-    fun createMediaStreamTrack(track: Long): MediaStreamTrack {
-        return checkNotNull(MediaStreamTrack.createMediaStreamTrack(track)) { "nativeTrack may not be null" }
-    }
+class RTCUtils {
+    companion object {
+        fun createMediaStreamTrack(track: Long): MediaStreamTrack {
+            return checkNotNull(MediaStreamTrack.createMediaStreamTrack(track)) { "nativeTrack may not be null" }
+        }
 
-    fun getNativeMediaStreamTrack(mediaStreamTrack: MediaStreamTrack): Long {
-        return mediaStreamTrack.nativeMediaStreamTrack
+        fun getNativeMediaStreamTrack(mediaStreamTrack: MediaStreamTrack): Long {
+            return mediaStreamTrack.nativeMediaStreamTrack
+        }
+
+        fun genRtpEncodingParameters(
+            rid: String?,
+            active: Boolean,
+            bitratePriority: Double?,
+            networkPriority: Int?,
+            maxBitrateBps: Int?,
+            minBitrateBps: Int?,
+            maxFramerate: Int?,
+            numTemporalLayers: Int?,
+            scaleResolutionDownBy: Double?,
+            ssrc: Long?,
+            adaptiveAudioPacketTime: Boolean?
+        ): RtpParameters.Encoding? {
+            return RtpParameters.Encoding(
+                rid,
+                active,
+                bitratePriority!!,
+                networkPriority!!,
+                maxBitrateBps,
+                minBitrateBps!!,
+                maxFramerate,
+                numTemporalLayers,
+                scaleResolutionDownBy,
+                ssrc,
+                adaptiveAudioPacketTime!!
+            )
+        }
     }
 }
