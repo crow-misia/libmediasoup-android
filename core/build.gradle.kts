d7d20d8aad5e0d200ec8f4ff2beac5d4ba5f912c
ishabodiwala <97020104+ishabodiwala@users.noreply.github.com>
2022-06-17 11:49:20 +0530

    remove publishing from maven

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,30 +1,12 @@
 import org.jetbrains.dokka.gradle.DokkaTask
-import java.net.URI
 
 plugins {
     id("com.android.library")
     id("kotlin-android")
     id("org.jetbrains.dokka")
-    id("maven-publish")
-    id("signing")
 }
 
-object Maven {
-    const val groupId = "io.github.crow-misia.libmediasoup-android"
-    const val artifactId = "libmediasoup-android"
-    const val name = "libmediasoup-android"
-    const val desc = "mediasoup client side library for Android"
-    const val version = "0.9.0"
-    const val siteUrl = "https://github.com/crow-misia/libmediasoup-android"
-    const val gitUrl = "https://github.com/crow-misia/libmediasoup-android.git"
-    const val githubRepo = "crow-misia/libmediasoup-android"
-    const val licenseName = "The Apache Software License, Version 2.0"
-    const val licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-    const val licenseDist = "repo"
-}
-
-group = Maven.groupId
-version = Maven.version
+apply(from = "../publish.gradle")
 
 android {
     buildToolsVersion = "32.0.0"
@@ -154,78 +136,6 @@ val javadocJar by tasks.creating(Jar::class) {
     description = "Assembles JavaDoc JAR"
     archiveClassifier.set("javadoc")
     from(customDokkaTask.outputDirectory)
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                from(components.getByName("release"))
-
-                groupId = Maven.groupId
-                artifactId = Maven.artifactId
-
-                println("""
-                    |Creating maven publication
-                    |    Group: $groupId
-                    |    Artifact: $artifactId
-                    |    Version: $version
-                """.trimMargin())
-
-                artifact(sourcesJar)
-                artifact(javadocJar)
-
-                pom {
-                    name.set(Maven.name)
-                    description.set(Maven.desc)
-                    url.set(Maven.siteUrl)
-
-                    scm {
-                        val scmUrl = "scm:git:${Maven.gitUrl}"
-                        connection.set(scmUrl)
-                        developerConnection.set(scmUrl)
-                        url.set(Maven.gitUrl)
-                        tag.set("HEAD")
-                    }
-
-                    developers {
-                        developer {
-                            id.set("crow-misia")
-                            name.set("Zenichi Amano")
-                            email.set("crow.misia@gmail.com")
-                            roles.set(listOf("Project-Administrator", "Developer"))
-                            timezone.set("+9")
-                        }
-                    }
-
-                    licenses {
-                        license {
-                            name.set(Maven.licenseName)
-                            url.set(Maven.licenseUrl)
-                            distribution.set(Maven.licenseDist)
-                        }
-                    }
-                }
-            }
-        }
-        repositories {
-            maven {
-                val releasesRepoUrl = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-                val snapshotsRepoUrl = URI("https://oss.sonatype.org/content/repositories/snapshots")
-                url = if (Maven.version.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-                val sonatypeUsername: String? by project
-                val sonatypePassword: String? by project
-                credentials {
-                    username = sonatypeUsername.orEmpty()
-                    password = sonatypePassword.orEmpty()
-                }
-            }
-        }
-    }
-
-    signing {
-        sign(publishing.publications.getByName("maven"))
-    }
 }
 
 tasks {
