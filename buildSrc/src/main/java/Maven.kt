2b35d4c10b9b11ff5487041546e43f115c5cb222
Zenichi Amano <crow.misia@gmail.com>
2025-02-07 06:14:37 +0900

    chore: bump version 0.18.0 -> 0.18.1

--- buildSrc/src/main/java/Maven.kt
+++ buildSrc/src/main/java/Maven.kt
@@ -2,7 +2,7 @@ object Maven {
     const val GROUP_ID = "io.github.crow-misia.libmediasoup-android"
     const val ARTIFACT_ID = "libmediasoup-android"
     const val DESCRIPTION = "mediasoup client side library for Android"
-    const val VERSION = "0.18.0"
+    const val VERSION = "0.18.1"
     const val GITHUB_REPOSITORY = "crow-misia/libmediasoup-android"
     const val LICENSE_NAME = "The Apache Software License, Version 2.0"
     const val LICENSE_URL = "http://www.apache.org/licenses/LICENSE-2.0.txt"
